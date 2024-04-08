854b8770828a0bfcce5a612dde48c076f5d64eb9
Juan Cruz <juancruzliberati@hotmail.com>
2024-04-08 01:57:44 -0300

    added controller productController + viewProduct

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -5,21 +5,20 @@
 
 ],
 [
-'name'=>'Familias',
+'name'=>'Productos',
 ]
-
 ]">
 
     <!-- DEFINO EL VALOR DEL SLOT  en Name-->
     <x-slot name="action">
 
-        <a  href="{{route('admin.families.create')}}" class="btn btn-gray">
+        <a  href="{{route('admin.products.create')}}" class="btn btn-gray">
             Nuevo
         </a>
 
 
     </x-slot>
-    @if($families->count())
+    @if($products!=null &&  $products->count())
         <div class="relative overflow-x-auto">
             <table class="w-full text-sm text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-left text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -28,25 +27,36 @@
                         ID
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Name
+                        Sku
                     </th>
                     <th scope="col" class="px-6 py-3">
-
+                        Nombre
                     </th>
+
+
+                    <th scope="col" class="px-6 py-3">
+                        Precio
+                    </th>
+
+
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($families as $unaFamilia)
+                @foreach($products as $unP)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
 
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{$unaFamilia->id}}
+                            {{$unP->id}}
                         </th>
                         <td class="px-6 py-4">
-                            {{$unaFamilia->name}}
+                            {{$unP['sku']}}
                         </td>
                         <td class="px-6 py-4">
-                            <a href="{{route('admin.families.edit',$unaFamilia)}}"> Editar</a>
+                            {{$unP['name']}}
+                        </td>
+
+                        <td class="px-6 py-4">
+                            <a href="{{route('admin.products.edit',$unP)}}"> Editar</a>
                         </td>
 
                     </tr>
@@ -59,24 +69,26 @@
 
 
         <div class="mt-4">
-            {{$families->links()}}
+            {{$products->links()}}
+            {{--links() metodo de paginate--}}
         </div>
     @else
-        <div class="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        <div
+            class="flex items-center p-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+            role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
             </svg>
             <span class="sr-only">Info</span>
             <div>
                 <div>
-                    <span class="font-medium">Info Alert!</span>
+                    <span class="font-medium">Info Alert!</span> Todavia no hay productos registrados
                 </div>
-
-
 
     @endif
 
 
 </x-admin-layout>
-
 
