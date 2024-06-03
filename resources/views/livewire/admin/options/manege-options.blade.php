d6a8298417fa0fab9b21f9213d5448c357e5ec62
Juan Cruz <juancruzliberati@hotmail.com>
2024-06-02 23:53:59 -0300

    Display completed product options

--- resources/views/livewire/admin/options/manege-options.blade.php
+++ resources/views/livewire/admin/options/manege-options.blade.php
@@ -3,7 +3,16 @@
     <section class="rounded-lg bg-white shadow-lg">
 
         <header class="border-b border-gray-200 px-6 py-2">
-            <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
+            <div class="flex justify-between">
+                {{--justify-between, un elemento ext. izquierdo y el otro ext. derecho --}}
+
+                <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
+                {{-- generar una accion con click--}}
+                {{-- $set() es un metodo magico, permite cambiar el valor de una variable--}}
+                <x-button wire:click="$set('openModal',true)">Nuevo</x-button>
+            </div>
+
+
         </header>
 
 
@@ -33,8 +42,9 @@
                                     @case(1)
                                         {{-- Talla --}}
                                         {{-- si el usuario seleciona la opcion uno (talla), sus correspondencia (Relaciones), van a estar dentro del badge Dark --}}
+
                                         <span
-                                        <span class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+                                            class="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
                                            {{$feature->description}}
                                             </span>
 
@@ -43,7 +53,9 @@
                                         @break
                                     @case(2)
                                         {{-- Color --}}
-                                    <span class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4" style="background-color: {{$feature->value}}">
+                                        <span
+                                            class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4"
+                                            style="background-color: {{$feature->value}}">
 
 
 
@@ -67,5 +79,166 @@
 
     </section>
 
+    {{-- Utilizo un componentes de jetStream  --}}
+    {{-- llamo al componente x-dialog-modal, utiliza 3 parametros, se los paso como slot --}}
+
+    <x-dialog-modal wire:model="openModal">
+        {{--Defino el primer slot, hace referencia al slot con el nombre 'title' --}}
+
+        <x-slot name="title"> Crear nueva Opcion</x-slot>
+
+        {{--Defino el segundo slot, hace referencia al slot con el nombre 'content' --}}
+        <x-slot name="content">
+            <x-validation-errors class="mb-4"/>
+            <div class="grid grid-cols-2 gap-6 mb-4">
+                {{-- Creo una grilla con dos columnas y con una separacion de 6 --}}
+                <div>
+                    <x-label class="mb-1">Nombre</x-label>
+                    {{-- newOption.name es una referencia al atributo public $newOption['name'], se envia data al backend--}}
+                    <x-input class="w-full" wire:model="newOption.name" placeholder="Por ejemplo: Tamanio, Color.."/>
+
+
+                </div>
+                <div>
+                    <x-label class="mb-1"> Tipo</x-label>
+                    {{-- utiizo el componente x-select propio. lista desplegable --}}
+                    <x-select class="w-full" wire:model.live="newOption.type">
+                        <option value="1">Texto</option>
+                        <option value="2">Color</option>
+                    </x-select>
+                </div>
+            </div>
+            <div class="flex items-center mb-4">
+
+                {{-- flex permite que se expanda--}}
+                {{-- items-center, centra todos los componentes --}}
+                {{-- flex-1 ocupa el mayor espacio disponible --}}
+                <hr class="flex-1">
+                <span class="mx-4">
+                        Valores
+                    </span>
+
+                <hr class="flex-1">
+            </div>
+
+            {{-- space (espaciador en altura y distancia 4 --}}
+            <div class="mb-4 space-y-4">
+
+                @foreach($newOption['features'] as $index => $aFeature)
+
+                    {{-- utilizo el index, porque un index[0]= inputValue + inputDescription --}}
+                    {{-- se iterado por la cantidad de Features que tiene un producto. ejemplo: bolsos, tiene color celeste de tipo texto
+                    y ademas una 3 descripciones
+                    index[0]= value=> 3500 y description=> Celeste cielo.
+                     index[1]= value=> 7000 y description=> cuero y color celeste.
+                      index[0]= value=> 2000 y description=> Marron.
+                    --}}
+                    <div class="p-6 rounded-lg border border-gray-200 relative" wire:key="features-{{$index}}">
+                        {{-- agrego una llave al elemento que es iterado por foreach en livewire--}}
+                        {{-- dentro de la caja, agrego dos inputs, utlizando un grid --}}
+
+                        <div class="absolute -top-3 px-4 bg-white">
+                            <button wire:click="removeFeature({{$index}})">
+                                <i class="fa-solid fa-trash-can text-red-500 hover:text-red-600"></i>
+                            </button>
+                        </div>
+                        <div class="grid grid-cols-2 gap-6">
+
+                            <div>
+
+                                <x-label class="mb-1">
+                                    Valor
+                                </x-label>
+
+
+                                {{-- inputs n1, corresponde a la columna n1  --}}
+
+
+
+                                {{-- inputs n2, corresponde a la columna n2  --}}
+
+
+                                @switch($newOption['type'])
+
+                                    @case(1)
+                                        {{-- si case es 1, se muestra el input de texto- --}}
+                                        <x-input class="w-full" wire:model="newOption.features.{{$index}}.value"
+                                                 placeholder="Tamanio: ejemplo 3LLL"/>
+                                        @break
+                                    @case(2)
+                                        {{-- obtuve el 42 pixeles de la inspeccion, agrego esa misma configuracion con h-[42px]--}}
+                                        {{-- flex items-center, centro los objetos y con padding p-3 una separacion con la caja y el objeto --}}
+                                        <div
+                                            class="border border-gray-300 h-[42px] rounded-md flex items-center p-3 flex justify-between">
+                                            {{-- si case es 2, se muestra el input de color- --}}
+                                            {{$newOption['features'][$index]['value'] ?: 'Selecione un color'}}
+                                            {{-- ?: --}}
+                                            {{-- objeto ?(verifica que la variable este definida) :(verifica que tenga valor no nulo) 'una sentencia '  --}}
+                                            <x-input type="color"
+                                                     wire:model.live="newOption.features.{{$index}}.value"
+                                                     placeholder="Color: lila3000"/>
+
+                                        </div>
+                                        @break
+
+                                    @default
+                                @endswitch
+
+
+                            </div>
+                            <div>
+
+                                <x-label class="mb-1">
+                                    Descripcion
+                                </x-label>
+
+                                <x-input class="w-full" wire:model="newOption.features.{{$index}}.description"
+                                         placeholder="Tamanio: ejemplo 3LLL"/>
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                @endforeach
+
+            </div>
+            <div class="flex justify-end">
+                {{-- posiciono el contenido a la derecha --}}
+                {{-- addFeature es un metodo que va ser llamado (o ejecutado), cuando se haga uso del boton--}}
+                <x-button wire:click="addFeature" class="me-2">Agregar +
+
+                </x-button>
+
+
+            </div>
+        </x-slot>
+        {{--Defino el tercer slot, hace referencia al slot con el nombre 'footer' --}}
+        <x-slot name="footer">
+
+                <button class="btn btn-blue" wire:click="addOpt">
+                        Registrar Nuevo
+                </button>
+
+
+        </x-slot>
+
+    </x-dialog-modal>
+    {{--@dump($newOption)--}}
 
 </div>
+
+{{--
+    @isset($newOption['features'])
+                    @php
+                        $features = $newOption['features'];
+                        $length = count($features)-1;
+                        //dd($length);
+                   // dd($newOption['features'][1]);
+                    @endphp
+                    @for($index=0; $index < $length; $index++)
+                        {{--   {{ $i }}: {{ $features[$i]['value'] ?? '' }} - {{ $features[$i]['description'] ?? '' }} --}}
+
+
