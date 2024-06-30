cff37ac20dae855cf4b8b760600759016043aa41
Juan Cruz <juancruzliberati@hotmail.com>
2024-06-29 22:04:18 -0300

    funciones recursivas

--- resources/views/livewire/admin/products/product-variants.blade.php
+++ resources/views/livewire/admin/products/product-variants.blade.php
@@ -73,21 +73,23 @@
                                                 class="inline-block h-6 w-6 shadow-lg rounded-full border-2 border-gray-300 mr-4"
                                                 style="background-color: {{$feature['value']}}">
 
-                                    </span>
-                                                <button
 
-                                                    class="absolute z-10 left-3 -top-2 rounded-full bg-red-500 hover:bg-red-600 h-4 w-4 flex justify-center item-center"
-                                                    {{-- item-center mantengo la x en el centro del circulo --}}
-                                                    onclick=confirmDeleteFeature({{$option->id}},{{$feature['id']}})"
-                                                //confirmDeleteFeature({{$option->id}},{{$feature['id']}})
-                                                {{-- wire:click="deleteFeature({{$feature->id}})" --}}>
+                                                <button type="button"
+
+                                                        class="absolute z-10 left-3 -top-2 rounded-full bg-red-500 hover:bg-red-600 h-4 w-4 flex justify-center item-center"
+                                                        {{-- item-center mantengo la x en el centro del circulo --}}
+                                                        onclick="confirmDeleteFeature({{$option->id}},{{$feature['id']}})">
+                                                {{--confirmDeleteFeature({{$option->id}},{{$feature['id']}}) --
+                                                {{-- wire:click="deleteFeature({{$feature->id}})" --}}
                                                 <i class="fa-solid fa-xmark text-white text-xs"></i>
 
                                                 </button>
+                                                   </span>
 
                                             </div>
                                             @break
                                         @default
+                                            @break
 
                                     @endswitch
 
@@ -118,118 +120,119 @@
 
 
                 </div>
+                <div>
 
-                <x-dialog-modal wire:model="openModal">
-                    {{-- Defino los slots requeridos para el componente x-dialog-modal --}}
-                    <x-slot name="title">Agregar nueva opción</x-slot>
+                    <x-dialog-modal wire:model="openModal">
+                        {{-- Defino los slots requeridos para el componente x-dialog-modal --}}
+                        <x-slot name="title">Agregar nueva opción</x-slot>
 
-                    <x-slot name="content">
-                        {{-- Defino el contenido del modal --}}
-                        {{-- Defino los mensajes de validaciones --}}
-                        <x-validation-errors class="mb-4"/>
-                        <div class="mb-4">
-                            <x-label class="mb-1">Opción</x-label>
-                            <x-select class="w-full" wire:model.live="variantsSelect.option_id">
-                                {{--PARTE 1) wire.live="variantsSelect.option_id almacena el valor (option_id) selecionado por el usuario --}}
+                        <x-slot name="content">
+                            {{-- Defino el contenido del modal --}}
+                            {{-- Defino los mensajes de validaciones --}}
+                            <x-validation-errors class="mb-4"/>
+                            <div class="mb-4">
+                                <x-label class="mb-1">Opción</x-label>
+                                <x-select class="w-full" wire:model.live="variantsSelect.option_id">
+                                    {{--PARTE 1) wire.live="variantsSelect.option_id almacena el valor (option_id) selecionado por el usuario --}}
 
 
-                                <option value="" disabled selected>Seleciona una opcion</option>
-                                @foreach($options as $option)
-                                    {{-- Muestra de una lista desplegable (down drop list)--}}
-                                    <option value="{{$option->id}}">{{ $option->name }}</option>
-                                @endforeach
-                            </x-select>
-                        </div>
-                        <div class="flex items-center mb-6">
-                            {{-- Centra los items--}}
-                            <hr class="flex-1">
-                            {{-- crea una linea --}}
+                                    <option value="" disabled selected>Seleciona una opcion</option>
+                                    @foreach($options as $option)
+                                        {{-- Muestra de una lista desplegable (down drop list)--}}
+                                        <option value="{{$option->id}}">{{ $option->name }}</option>
+                                    @endforeach
+                                </x-select>
+                            </div>
+                            <div class="flex items-center mb-6">
+                                {{-- Centra los items--}}
+                                <hr class="flex-1">
+                                {{-- crea una linea --}}
 
-                            <span class="mx-4">Valores</span>
-                            <hr class="flex-1">
-                            {{-- crea la segunda linea --}}
-                        </div>
-                        {{-- la etiqueta ul se encarga de mostrar todos los 'valores', al ejecutar el "Valor +" entonces se recarga y vuelve a interacion
-                         mostrando los nateriores + el ultimo --}}
-                        <ul class="mb-4 space-y-4">
-                            {{-- con space-y-4 genera un espacio vertical por cada elemento de lista de UL --}}
-                            {{-- aca van todos los features de la variable computada--}}
-                            @foreach($variantsSelect['features'] as $index=> $feature)
-                                {{-- (contiene la posicion) $index => $feature (contiene el valor en esa posicion) --}}
-                                <li wire:key="variantsSelect-feature-.{{$index}}"
-                                    class="border border-gray-200 rounded-lg p-6 relative">
+                                <span class="mx-4">Valores</span>
+                                <hr class="flex-1">
+                                {{-- crea la segunda linea --}}
+                            </div>
+                            {{-- la etiqueta ul se encarga de mostrar todos los 'valores', al ejecutar el "Valor +" entonces se recarga y vuelve a interacion
+                             mostrando los nateriores + el ultimo --}}
+                            <ul class="mb-4 space-y-4">
+                                {{-- con space-y-4 genera un espacio vertical por cada elemento de lista de UL --}}
+                                {{-- aca van todos los features de la variable computada--}}
+                                @foreach($variantsSelect['features'] as $index=> $feature)
+                                    {{-- (contiene la posicion) $index => $feature (contiene el valor en esa posicion) --}}
+                                    <li wire:key="variantsSelect-feature-.{{$index}}"
+                                        class="border border-gray-200 rounded-lg p-6 relative">
 
-                                    <div class="absolute -top-3 bg-white px-4">
-                                        <button wire:click="removeFeature({{$index}})">
-                                            <i class="fa-solid fa-trash-can text-red-500 hover:text-red-600"></i>
+                                        <div class="absolute -top-3 bg-white px-4">
+                                            <button wire:click="removeFeature({{$index}})">
+                                                <i class="fa-solid fa-trash-can text-red-500 hover:text-red-600"></i>
 
-                                        </button>
+                                            </button>
 
-                                    </div>
+                                        </div>
 
-                                    <div>
-                                        <x-label class="mb-1">Valores</x-label>
-                                        {{--Para enviar la respuesta del usuario de la lista drop down list al componente ProductVariants
-                                        debbemos enlazar la respuesta, se la siguiente manera.
-                                        wire:model="$variantsSelect.features.{{$index}}.id
-                                        Almena el valor de la lista desplegable a la variable $variantsSelect.features.{{$index}}.id
-                                        --}}
+                                        <div>
+                                            <x-label class="mb-1">Valores</x-label>
+                                            {{--Para enviar la respuesta del usuario de la lista drop down list al componente ProductVariants
+                                            debbemos enlazar la respuesta, se la siguiente manera.
+                                            wire:model="$variantsSelect.features.{{$index}}.id
+                                            Almena el valor de la lista desplegable a la variable $variantsSelect.features.{{$index}}.id
+                                            --}}
 
-                                        <x-select class="w-full" wire:model="variantsSelect.features.{{$index}}.id"
-                                                  wire:change="feature_change({{$index}})">
-                                            <option value="">
-                                                Selecione un valor
-                                            </option>
-                                            @foreach($this->features as $featurBD)
-
-                                                <option value="{{ $featurBD->id }}">
-                                                    {{$featurBD->description}}
-
+                                            <x-select class="w-full" wire:model="variantsSelect.features.{{$index}}.id"
+                                                      wire:change="feature_change({{$index}})">
+                                                <option value="">
+                                                    Selecione un valor
                                                 </option>
-                                            @endforeach
+                                                @foreach($this->features as $featurBD)
+
+                                                    <option value="{{ $featurBD->id }}">
+                                                        {{$featurBD->description}}
+
+                                                    </option>
+                                                @endforeach
 
 
-                                        </x-select>
+                                            </x-select>
 
-                                    </div>
-
-
-                                </li>
-                            @endforeach
+                                        </div>
 
 
-                        </ul>
-                        <div class="flex justify-end">
-
-                            <x-button wire:click="addFeature">
-                                Valor +
-                            </x-button>
-
-                        </div>
+                                    </li>
+                                @endforeach
 
 
-                    </x-slot>
+                            </ul>
+                            <div class="flex justify-end">
 
-                    <x-slot name="footer">
-                        <div>
+                                <x-button wire:click="addFeature">
+                                    Valor +
+                                </x-button>
 
-                            <x-danger-button wire:click="$set('openModal',false)">
-                                Cancelar
-                            </x-danger-button>
-                            <x-button wire:click="saveFeature" class="ml-2">
-                                Guardar
-                            </x-button>
-                        </div>
-                    </x-slot>
-                </x-dialog-modal>
-        </div>
+                            </div>
+
+
+                        </x-slot>
+
+                        <x-slot name="footer">
+                            <div>
+
+                                <x-danger-button wire:click="$set('openModal',false)">
+                                    Cancelar
+                                </x-danger-button>
+                                <x-button wire:click="saveFeature" class="ml-2">
+                                    Guardar
+                                </x-button>
+                            </div>
+                        </x-slot>
+                    </x-dialog-modal>
+                </div>
 
     </section>
 
     @push('js')
         <script>
             function confirmDeleteFeature(option_id, feature_id) {
-
+                alert(option_id);
                 //muesto el id del elemento a eliminar -> alert(featureId);
                 Swal.fire({
                     title: "Esta seguro ?",
@@ -276,5 +279,5 @@
         </script>
 
     @endpush
-
+  </div>
 </div>
