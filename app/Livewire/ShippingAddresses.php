f14c0ce62c2cc1462f7bae4fb2a30cb8b0b2907f
Juan Cruz <juancruzliberati@hotmail.com>
2024-07-27 20:57:14 -0300

    revisar error tipo a la hora de crear una instancia

--- app/Livewire/ShippingAddresses.php
+++ app/Livewire/ShippingAddresses.php
@@ -4,7 +4,9 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Address;
+use App\Models\User;
 use App\Livewire\Forms\CreateAddressForm;
+
 class ShippingAddresses extends Component
 {
     public $addresses;
@@ -13,12 +15,45 @@ class ShippingAddresses extends Component
     //createAddress es una instancia de la clase CreateAddress que se encarga de crear una nueva dirrecion
     public CreateAddressForm $createAddress;
 
-    public function mount(){
+    public function mount()
+    {
         //recuperar todas las dirreciones del usuario autenticado al momento de instanciar el componente ShippingAddresses
         $this->addresses = Address::where('user_id', auth()->id())->get();
 
 
+        //accedo a la propiedad receiver_info de la instancia createAddress y le asigno un array con datos
+        $this->createAddress->receiver_info = [
+            //dd(auth()->user()->toArray()),
+            //recupero  el nombre del usuario autenticado.
+            'name' => auth()->user()->name,
+            //recupero  el nombre del usuario autenticado.
+            'last_name' => auth()->user()->last_name,
+            //recupero  el tipo de documento del usuario autenticado.
+            'document_type' => auth()->user()->document_type,
+            //recupero  el numero de documento del usuario autenticado.
+            //'document_number' => auth()->user()->document_number,
+            'document_number' => (string) auth()->user()->document_number,
+            //recupero  el numero de telefono del usuario autenticado.
+            'phone' => auth()->user()->phone,
+
+
+        ];
+        //dd($this->createAddress->receiver_info);
+
+
     }
+    public function store(){
+        // se crear una nueva instancia de dirreccion, se almacena en la bd+ limpia y se recupera la informacion del usuario autenticado
+        $this->createAddress->save();
+        // refrezco los datos de las dirreciones del usuario autenticado en la vista
+        $this->addresses = Address::where('user_id', auth()->id())->get();
+        // despues de crear una direccion, cierro el formulario con $this->newAddress = false;
+        $this->newAddress = false;
+
+
+
+    }
+
     public function render()
     {
         return view('livewire.shipping-addresses');
--- resources/views/livewire/shipping-addresses.blade.php
+++ resources/views/livewire/shipping-addresses.blade.php
@@ -11,6 +11,11 @@
         <div class="p-4">
 
             @if($newAddress)
+                {{-- muestro los posibles errores de validaciones  --}}
+            <x-validation-errors class="mb-6">
+
+            </x-validation-errors>
+
                 {{-- aca se muestra el formulario para crear una nueva instancia de dirreccion --}}
                 <div class="grid grid-cols-4 gap-4">
                     {{-- grid  de un ancho de 4 columnas con una separacion de 4 entre ellas --}}
@@ -21,8 +26,8 @@
                             {{-- accedo a la instancia de CreateAddressForm y a su valor type con la sentencia createAddress.type--}}
                             {{-- con el objetivo de mostrar en la lista, los valores de 'option', segun el valor seleciondo por el usuario en x-select --}}
                             {{-- ejemplo: si  createAddress.type es 1 entonces se muestra la option de 2 con el valor de Oficina --}}
-                            {{--  segui en -09: 49 --}}
-                            <option value="">
+
+                            <option value="0">
                                 Tipo de direccion
 
                             </option>
@@ -65,35 +70,80 @@
                 </div>
                 {{--  fin de la grid de 4 cols--}}
                 <hr class="my-4">
-                <div class="font-semibold mb-2">
-                    <p>
+
+                <div x-data="{
+                receiver:@entangle('createAddress.receiver'),
+                {{-- enlazo una de las propiedades de clase CreateAddressForm.receiver a traves del componente ShippingAddress que tiene una instancia de CreateAddresForm createAddress.receiver --}}
+                {{-- esto se logra se la siguiente manera: unaVariableAlPine: @entangle('createAddress.receiver'),
+                 comunicacion bidireccional   unaVariableAlpine <---->receiver en segundo plano
+                 objetivo: comunicacion fluida en segundo plano --}}
+                 receiver_info:@entangle('createAddress.receiver_info'),
+                 {{-- enlazo la propiedad el atributo de tipo arreglo receiver_info de la instancia CreateAddressForm en la variable alpine 'receiver_info', con esto obtenemos toda la info  --}}
+                }" x-init="$watch('receiver',value => {
+
+                if(value==1){
+                //cambio a sere yo, recupero la informacion del usuario autenticado con  {{--'{{ auth()->user()->propiedadesDelUsuario }}'} --}}'
+                  receiver_info.name ='{{auth()->user()->name}}';
+                  receiver_info.last_name ='{{auth()->user()->last_name}}';
+                  receiver_info.document_type='{{auth()->user()->document_type}}';
+                  receiver_info.document_number ='{{auth()->user()->document_number}}';
+                  receiver_info.phone ='{{auth()->user()->phone}}';
+
+                }else{
+                //cambio a otra persona, por lo tanto borro todos los datos precargados
+                  receiver_info.name = '';
+                  receiver_info.last_name ='';
+                  receiver_info.document_number ='';
+                  receiver_info.phone ='';
+                }
+
+                })"
+
+                    {{-- para detectar cambio de una propiedad de alpine y que haga accion,
+                  utilizo x-init y el metodo $watch('propiedadAlPineEscuchandoCambios',nuevoValor => {
+                  if(nuevoValor==1){ //cambio a sere yo
+                  }eslse{
+                  //cambio a otra persona, por lo tanto borro todos los datos precargados
+                  receiver_info.name = '';
+                  receiver_info.last_name ='';
+                  receiver_info.document_number ='';
+                  receiver_info.phone ='';
+                  })   --}}
+                >
+                    {{-- inicializo alpine --}}
+                    <p class="font-semibold mb-2">
                         Quien recibira el pedido
                     </p>
                     <div class="flex space-x-4 mb-4">
                         <label class="flex items-center">
-                            <input type="radio" value="1" class="mr-2">
+                            {{--enlazo la propiedad de alpine (receiver) con los inputs y mostrar ese valor con  <input x-model="receiver" --}}
+                            {{--  value == 1 entonces sere yo --}}
+                            <input x-model="receiver" type="radio" value="1" class="mr-2">
                             Sere yo </label>
                         <label items-center>
-                            <input type="radio" value="0" class="mr-2">
+                            {{--  value == 0 entonces Otra persona --}}
+                            <input x-model="receiver" type="radio" value="0" class="mr-2">
                             Otra persona</label>
                     </div>
                     {{-- grid de 2 columnas --}}
                     <div class="grid grid-cols-2 gap-2">
                         {{-- nombres --}}
                         <div>
-                            <x-input class="w-full" placeholder="nombres">
+                            {{-- x-bind: enlazo el atributo alpine con html, y el metodo disabled lo habilita/desabilita segun el valor. Ejemplo: x-bind:disabled="receiver==1" entrada de datos bloqueada --}}
+                            <x-input x-bind:disabled="receiver==1" x-model="receiver_info['name']" class="w-full" placeholder="nombres">
 
                             </x-input>
                         </div>
                         {{-- apellidos --}}
                         <div>
-                            <x-input class="w-full" placeholder="apellidos">
+                            <x-input x-bind:disabled="receiver==1" x-model="receiver_info['last_name']" class="w-full" placeholder="apellidos">
 
                             </x-input>
                         </div>
                         {{-- Selecionar un tipo de documento --}}
                         <div class="flex space-x-2">
-                            <x-select name="" id="">
+                            {{-- enlazo la propiedad alpine en x-select para mostrar el valor --}}
+                            <x-select x-bind:disabled="receiver==1" x-model="receiver_info['document_type']" name="" id="">
                                 @foreach(\App\Enums\TypeOfDocuments::cases() as $unEnums)
                                     {{-- para mostrar $unEnum, utlizo <option> --}}
                                     <option value="{{$unEnums->value}}">{{$unEnums->name}}</option>
@@ -101,24 +151,28 @@
                                 {{-- numero de documento --}}
 
                             </x-select>
-                            <x-input class="w-full" type="text" placeholder="ingrese el numero"/>
+                            <x-input x-bind:disabled="receiver==1" x-model="receiver_info['document_number']" class="w-full" type="text"
+                                     placeholder="ingrese el numero"/>
 
 
                         </div>
                         {{-- numero de telefono --}}
                         <div>
-                            <x-input type="text" placeholder="telefono" class="w-full"/>
+                            <x-input x-bind:disabled="receiver==1" x-model="receiver_info['phone']" type="text" placeholder="telefono"
+                                     class="w-full"/>
                         </div>
                         {{-- botton para cancelar direccion --}}
-                        <div class="btn btn-outline-gray w-full">
-                                <button>
-                                    Cancelar
-                                </button>
+                        <div class="btn btn-outline-gray w-full flex justify-center items-center" wire:click="$set('newAddress',false)">
+                            <button class="center-items">
+                                Cancelar
+                            </button>
                         </div>
 
                         {{-- botton para guardar direccion --}}
-                        <div class="btn btn-purple w-full">
-                            <button>
+
+                        <div class="btn btn-purple w-full flex justify-center items-center">
+                            {{-- cuando se haga en el boton Guardar se ejecuta un metodo del componente llamado store--}}
+                            <button wire:click="store" class="center-items">
                                 Guardar
                             </button>
                         </div>
